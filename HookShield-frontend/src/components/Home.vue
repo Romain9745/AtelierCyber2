960b444a646ae6543090e7b789abfa93a54f324b
ARGAUD <romain943602@gmail.com>
2025-02-03 09:13:37 -0500

    init project

---
+++ HookShield-frontend/src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="p-4 bg-gray-100 dark:bg-gray-900 min-h-screen">
+      <h1 class="text-2xl font-bold text-gray-900 dark:text-white mb-4">Tableau de Données</h1>
+      <Table :data="tableData" />
+    </div>
+  </template>
+  
+  <script>
+  import Table from "@/components/Table.vue";
+  
+  export default {
+    components: {
+      Table,
+      data() {
+    return {
+      tableData: [
+        { id: 1, name: 'Alice', age: 25, skills: ['Vue', 'React'], active: true },
+        { id: 2, name: 'Bob', age: 30, skills: ['Angular', 'TypeScript'], active: false },
+        { id: 3, name: 'Charlie', age: 28, skills: ['Svelte', 'Go'], active: true },
+        { id: 4, name: 'Diana', age: 22, skills: ['Python', 'Django'], active: false },
+      ],
+    };
+  },
+    },
+  };
+  </script>
---
+++ HookShield-frontend/src/components/Table.vue
@@ -0,0 +1,34 @@
+<template>
+    <table class="w-full border-collapse border border-gray-300 dark:border-gray-700">
+      <thead>
+        <tr class="bg-gray-200 dark:bg-gray-800">
+          <th v-for="(header, index) in headers" :key="index" class="border border-gray-300 dark:border-gray-700 p-2 text-left">
+            {{ header }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(row, rowIndex) in data" :key="rowIndex" class="odd:bg-white even:bg-gray-100 dark:odd:bg-gray-700 dark:even:bg-gray-800">
+          <td v-for="(value, colIndex) in row" :key="colIndex" class="border border-gray-300 dark:border-gray-700 p-2">
+            {{ value }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      data: {
+        type: Array,
+        required: true,
+      },
+    },
+    computed: {
+      headers() {
+        return this.data.length > 0 ? Object.keys(this.data[0]) : [];
+      },
+    },
+  };
+  </script>
